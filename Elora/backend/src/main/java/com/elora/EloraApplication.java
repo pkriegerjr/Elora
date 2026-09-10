29a824f670ec43b9b81b8689e78645e10f19325c
Pkriegerjr <143087551+pkriegerjr@users.noreply.github.com>
2026-09-10 19:37:15 +0000

    Refatoração do sistema

---
+++ Elora/backend/src/main/java/com/elora/EloraApplication.java
@@ -0,0 +1,11 @@
+package com.elora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EloraApplication.class, args);
+    }
+}
